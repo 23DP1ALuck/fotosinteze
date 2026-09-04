50bd2456af18139ede82f70255c0233cf55a7ea3
Artjoms Luckis <a231660al@rvt.lv>
2026-09-04 10:46:59 +0300

    feat: create database

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,25 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+from fastapi import FastAPI
+from app.config import settings
+from app.database import sessionmanager
+
+
+
+# inspired from
+# https://medium.com/@tclaitken/setting-up-a-fastapi-app-with-async-sqlalchemy-2-0-pydantic-v2-e6c540be4308
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Function that handles startup and shutdown events.
+    To understand more, read https://fastapi.tiangolo.com/advanced/events/
+    """
+    yield
+    if sessionmanager._engine is not None:
+        # Close the DB connection
+        await sessionmanager.close()
+
+app = FastAPI(lifespan=lifespan, title=settings.project_name)
 
 @app.get("/")
 def read_root():
